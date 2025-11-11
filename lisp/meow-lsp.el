921bb286e60df5732bffe89eaaffd294cc4e3f02
Thunder <thunder@disroot.org>
2025-11-11 12:40:07 +0200

    eglot rename keybind

--- lisp/meow-lsp.el
+++ lisp/meow-lsp.el
@@ -17,7 +17,9 @@
   (meow/leader
     "c" '(:ignore t :wk "code")
     "ca" '("code actions" . (lambda () (interactive)
-			      (eglot-code-actions 1 (point-max) nil t)))))
+			      (eglot-code-actions 1 (point-max) nil t))))
+  (:states '(normal visual insert)
+	   "M-r" #'eglot-rename))
 
 (use-package eglot-booster
   :after eglot
