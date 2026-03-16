0557d262e71ef11bdd51f1128406264a27f7f38f
Thunder <thunder@disroot.org>
2026-03-16 14:01:01 +0200

    bbbbbbbbbbbbbbb

--- lisp/meow-ewm.el
+++ lisp/meow-ewm.el
@@ -1,23 +1,23 @@
 ;; -*- lexical-binding: t; -*-
 (with-eval-after-load "ewm"
-  ;; (let ((local-ewm-file (expand-file-name "ewm-local.el" user-emacs-directory)))
-  ;;   (when (or (file-directory-p local-ewm-file)
-  ;;   		  (file-symlink-p local-ewm-file))
-  ;;     (load-file local-ewm-file)))
+  (let ((local-ewm-file (expand-file-name "ewm-local.el" user-emacs-directory)))
+    (when (or (file-directory-p local-ewm-file)
+    		  (file-symlink-p local-ewm-file))
+      (load-file local-ewm-file)))
 
-  ;; (setq ewm-input-config
-  ;; 		'((touchpad :natural-scroll t :tap t :dwt t)
-  ;; 		  (mouse :accel-profile "flat")
-  ;; 		  (keyboard :repeat-delay 300 :repeat-rate 50
-  ;; 					:xkb-layouts "us,fi"
-  ;; 					:xkb-options "grp:win_space_toggle")))
+  (setq ewm-input-config
+		'((touchpad :natural-scroll t :tap t :dwt t)
+		  (mouse :accel-profile "flat")
+		  (keyboard :repeat-delay 300 :repeat-rate 50
+					:xkb-layouts "us,fi"
+					:xkb-options "grp:win_space_toggle")))
 
-  ;; (setq ewm-mouse-follows-focus t)
+  (setq ewm-mouse-follows-focus t)
 
-  ;; (setq ewm-intercept-prefixes (mapcar (lambda (key)
-  ;; 										 (aref (kbd key) 0))
-  ;; 									   '("M-x"
-  ;; 										 "C-SPC")))
+  (setq ewm-intercept-prefixes (mapcar (lambda (key)
+										 (aref (kbd key) 0))
+									   '("M-x"
+										 "C-SPC")))
 
   ;; (ewm--send-input-config)
 
