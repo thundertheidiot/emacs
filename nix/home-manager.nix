15d9e89a0bcb4bf46eb2e19398b3a90241ba8275
Thunder <thunder@disroot.org>
2026-03-24 15:06:25 +0200

    home manager ewm portals

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -26,6 +26,18 @@ in {
       wl-clipboard
     ];
 
+    # ewm portals
+    # https://codeberg.org/ezemtsov/ewm/pulls/53
+    xdg.portal = {
+      enable = true;
+      config.ewm = {
+        default = "gnome;gtk;";
+        "org.freedesktop.impl.portal.Access" = "gtk";
+        "org.freedesktop.impl.portal.Notification" = "gtk";
+        "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+      };
+    };
+
     xdg.configFile."emacs/early-init.el" = {
       enable = true;
       source = ../early-init.el;
